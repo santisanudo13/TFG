d430b8ca2a7c0cb23455ae6349e7c2ea49584976
Santiago Sañudo <santisanudo@gmail.com>
2018-04-09 11:29:01 +0200

    asd

--- Mi libreria/linux_lib/src/uploadEjecutable.sh
+++ Mi libreria/linux_lib/src/uploadEjecutable.sh
@@ -3,6 +3,6 @@
 
 cd ~/TFG/Mi\ libreria/linux_lib/src/
 arm-linux-gnueabihf-gcc *.c -o main.o -Wl,--dynamic-linker=/data/local/lib/ld-linux.so.3 -Wl,-rpath=/data/local/lib -fPIE -pie
-adb connect 192.168.43.205
+adb connect 10.42.0.190
 adb push main.o /data/local/tmp
 
